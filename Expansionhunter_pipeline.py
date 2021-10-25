353f75e302984967f308c7c752a00aa034b113ab
GabevandenHoeven <gcw.vandenhoeven@student.han.nl>
2021-10-25 09:27:19 +0200

    commit voor het toevoegen van bash script op het hpc

---
+++ Expansionhunter_pipeline.py
@@ -0,0 +1,22 @@
+
+
+def read_file(filename):
+    """Reads a file
+    """
+
+    open(filename)
+
+
+def pipeline():
+    """Calls a shell script while looping over a dataset
+    """
+
+
+if __name__ == "__main__":
+    print("Starting pipeline...")
+    pc_dir = "/hpc/diaggen/users/Gabe/data/wes/Positive_controls/*"
+
+    #
+
+    # read_file("dataset.txt")
+
