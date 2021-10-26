48d28af6b6dd4f63bdc33efb9f9dea556b28a0fa
GabevandenHoeven <gcw.vandenhoeven@student.han.nl>
2021-10-26 17:36:29 +0200

    Expansionhunter_pipeline.py en expansionhunter_script.sh aangepast voor automatisch uitvoeren en in een oogopslag duidelijke output.

--- Expansionhunter_pipeline.py
+++ Expansionhunter_pipeline.py
@@ -1,22 +1,71 @@
+import os
+import subprocess
+
+
+def processing_pipeline(d):
+    """Processing results of vcf files while looping over a directory.
+    """
+    for filename in os.listdir(d):
+        if filename.endswith(".vcf"):
+            read_file(filename)
 
 
 def read_file(filename):
     """Reads a file
     """
+    lines = ["repeat_ID\treference\trepeats_length\tgenotype\n"]
+    with open(filename) as file:
+        for line in file:
+            if not line.startswith("#"):
+                rep_id = line.split("\t")[7].split(";")[5].removeprefix("REPID=")
+                ref = line.split("\t")[7].split(";")[1].removeprefix("REF=")
+                alt = line.split("\t")[4]
+                if not alt == ".":
+                    alt = alt.split(",")
+                    for e in alt:
+                        e = e.removeprefix("<STR").removesuffix(">")
+                genotype = line.split("\t")[9].split(":")[0]
+                if len(alt) == 1:
+                    lines.append(rep_id+"\t"+ref+"\t"+str(alt[0])+"\t"+genotype+"\n")
+                else:
+                    lines.append(rep_id+"\t"+ref+"\t"+alt[0]+","+alt[1]+"\t"+genotype+"\n")
+    new_filename = "/hpc/diaggen/users/Gabe/analysis/output_exhunt/" + filename.split("/")[-1].removesuffix(".vcf") \
+                   + "output.tsv"
+    with open(new_filename, "w") as file:
+        for line in lines:
+            file.write(line)
 
-    open(filename)
 
-
-def pipeline():
-    """Calls a shell script while looping over a dataset
+def analysis_pipeline(d):
+    """Calls a shell script while looping over a directory.
     """
+    for filename in os.listdir(d):
+        if filename.endswith(".bam"):
+            prefix = "/hpc/diaggen/users/Gabe/analysis/output_exhunt/" + filename.split("/")[-1].removesuffix(".bam")
+            if "CM" in prefix:
+                sex = "male"
+            else:
+                sex = "female"
+            subprocess.call(["expansionhunter_script.sh", filename, prefix, sex])
 
 
 if __name__ == "__main__":
-    print("Starting pipeline...")
-    pc_dir = "/hpc/diaggen/users/Gabe/data/wes/Positive_controls/*"
 
-    #
+    pipe = input()
 
-    # read_file("dataset.txt")
-
+    if pipe.upper() == "ANALYSE":
+        # Analysis pipeline
+        print("Starting analysis pipeline...")
+        pc_dir = "/hpc/diaggen/users/Gabe/data/wes/Positive_controls/"
+        giab_dir = "/hpc/diaggen/users/Gabe/data/wes/GIAB/"
+        analysis_pipeline(pc_dir)
+        analysis_pipeline(giab_dir)
+    elif pipe.upper() == "PROCESS":
+        # Processing pipeline
+        print("Starting processing pipeline...")
+        res_dir = "/hpc/diaggen/users/Gabe/analysis/output_exhunt/"
+        processing_pipeline(res_dir)
+    else:
+        print("Error. Pipeline command not recognised.")
+        print("Use \"ANALYSE\" to activate the analysis pipeline and process bam files using ExpansionHunter, "
+              "or use \"PROCESS\" to activate the processing pipeline to process ExpansionHunter output vcf files.")
--- expansionhunter_script.sh
+++ expansionhunter_script.sh
@@ -1,23 +1,22 @@
 #!/bin/bash
 #SBATCH --time=1:00:00
-#SBATCH --nodes=2
+#SBATCH --c=2
 #SBATCH --mem 5G
-#SBATCH --gres=tmpspace:10G
 #SBATCH --mail-user g.c.w.vandenhoeven-2@umcutrecht.nl
 #SBATCH --mail-type ALL
-#SBATCH --export=NONE
-#SBATCH --account=diaggen
 #
 # This is a script that will call ExpansionHunter to perform an analysis using variable input
 #
 # "reads" need to be in a BAM file
 # "output-prefix" is the name you want the output files to have
 # "sex" is either "female" -default- or "male"
+# Additional parameters
+#--region-extension-length int \
+
 
 /hpc/diaggen/users/Gabe/tools/ExpansionHunter-v5.0.0-linux_x86_64/bin/ExpansionHunter \
 --reads $1 \
 --reference /hpc/diaggen/data/databases/ref_genomes/Homo_sapiens.GRCh37.GATK.illumina/Homo_sapiens.GRCh37.GATK.illumina.fasta \
---variant-catalog /hpc/diaggen/users/Gabe/tools/ExpansionHunter-v5.0.0-linux_x86_64/variant_catalog/hg19/variant_catalog.json \
+--variant_catalog /hpc/diaggen/users/Gabe/tools/ExpansionHunter-v5.0.0-linux_x86_64/variant_catalog/hg19/variant_catalog.json \
 --output-prefix /hpc/diaggen/users/Gabe/analysis/output_exhunt/$2 \
---region-extension-length 1000 \
---sex $3 \
+--sex $3
