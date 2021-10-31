f808bbc4899d9247abb61cd3d2c4e34ef3488cae
GabevandenHoeven <gcw.vandenhoeven@student.han.nl>
2021-10-31 13:43:20 +0100

    Een begin gemaakt aan de pipeline voor lobSTR. LobSTR_pipeline.py en lobSTR_script.sh aangemaakt.

---
+++ LobSTR_pipeline.py
@@ -0,0 +1,34 @@
+import os
+import subprocess
+
+
+def analysis_pipeline(d):
+    """
+    """
+    for filename in os.listdir(d):
+        if filename.endswith(".bam"):
+            prefix = "/hpc/diaggen/users/Gabe/analysis/output_lobstr/allelotype/" + \
+                     filename.split("/")[-1].removesuffix(".bam")
+            subprocess.call(["sbatch lobSTR_script.sh", filename, prefix])
+
+
+def processing_pipeline(d):
+    """Processing results of vcf files while looping over a directory.
+    """
+    for filename in os.listdir(d):
+        if filename.endswith(".vcf"):
+            read_file(filename)
+
+
+def read_file(filename):
+    """"""
+
+    with open(filename) as file:
+        for line in file:
+            print("finish pipeline")
+
+
+if __name__ == "__main_":
+    print("Starting pipeline...")
+
+
---
+++ lobSTR_script.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#SBATCH --time=1:00:00
+#SBATCH --c=2
+#SBATCH --mem 5G
+#SBATCH --mail-user g.c.w.vandenhoeven-2@umcutrecht.nl
+#SBATCH --mail-type ALL
+
+# Dit is een script wat met twee input velden een lobSTR analyse draait voor een bam file.
+# parameters zijn:
+# $1 : absolute/path/bam_file.bam
+# $2 : absolute/path/output_prefix
+
+singularity shell -B /hpc/diaggen:/hpc/diaggen /hpc/diaggen/software/singularity_cache/lobster_v4.0.0.img
+
+allelotype \
+--command classify \
+--bam $1 \
+--noise_model /hpc/diaggen/users/Gabe/tools/lobSTR/models/illumina_v3.pcrfree \
+--strinfo /hpc/diaggen/users/Gabe/tools/lobSTR/lobstr_new/lobstr_v3.0.2_hg19_strinfo.tab \
+--index-prefix /hpc/diaggen/users/Gabe/tools/lobSTR/lobstr_new/lobstr_hg19_ref/lobSTR_ \
+--out $2
