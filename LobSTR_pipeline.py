082957f5b6401b14b789ca77db0bc53c302d163d
GabevandenHoeven <gcw.vandenhoeven@student.han.nl>
2021-11-01 16:26:37 +0100

    LobSTR_pipeline.py aangepast callen van singularity en allelotype nu in het python script ipv in een apart bash script.

--- LobSTR_pipeline.py
+++ LobSTR_pipeline.py
@@ -11,15 +11,15 @@ def analysis_pipeline(in_d, out_d):
     """
     for filename in os.listdir(in_d):
         if filename.endswith(".bam"):
-            # prefix = out_d + filename.split("/")[-1].removesuffix(".bam")
             prefix = out_d + filename.split("/")[-1].replace(".bam", "")
-            subprocess.run(f"singularity shell -B /hpc/diaggen:/hpc/diaggen "
-                           f"/hpc/diaggen/software/singularity_cache/lobster_v4.0.0.img")
-            subprocess.run(f"allelotype --command classify --bam {filename} "
-                           f"--noise_model /hpc/diaggen/users/Gabe/tools/lobSTR/models/illumina_v3.pcrfree "
-                           f"--strinfo /hpc/diaggen/users/Gabe/tools/lobSTR/lobstr_new/lobstr_v3.0.2_hg19_strinfo.tab "
-                           f"--index-prefix /hpc/diaggen/users/Gabe/tools/lobSTR/lobstr_new/lobstr_hg19_ref/lobSTR_ "
-                           f"--out {prefix}")
+            subprocess.run([f"singularity", f"shell", f"-B", f"/hpc/diaggen:/hpc/diaggen",
+                           f"/hpc/diaggen/software/singularity_cache/lobster_v4.0.0.img"])
+            subprocess.run([f"allelotype", f"--command", f"classify", f"--bam", f"{in_d}{filename}", f"--noise_model",
+                            f"/hpc/diaggen/users/Gabe/tools/lobSTR/models/illumina_v3.pcrfree",
+                            f"--strinfo", f"/hpc/diaggen/users/Gabe/tools/lobSTR/lobstr_new/"
+                                          f"lobstr_v3.0.2_hg19_strinfo.tab", f"--index-prefix",
+                            f"/hpc/diaggen/users/Gabe/tools/lobSTR/lobstr_new/lobstr_hg19_ref/lobSTR_",
+                            f"--out", f"{prefix}"])
 
 
 if __name__ == "__main_":
@@ -28,6 +28,3 @@ if __name__ == "__main_":
     # giab_dir = "/hpc/diaggen/users/Gabe/data/wes/GIAB/"
     analysis_pipeline(sys.argv[1], sys.argv[2])
     print("Analysis complete.")
-
-
-
