44d979d66d7741ec87a9070e8aa08a64f44b5be2
GabevandenHoeven <gcw.vandenhoeven@student.han.nl>
2021-11-01 14:51:21 +0100

    LobSTR_pipeline.py aangepast

--- LobSTR_pipeline.py
+++ LobSTR_pipeline.py
@@ -3,16 +3,15 @@ import subprocess
 import sys
 
 
-def analysis_pipeline(d):
+def analysis_pipeline(in_d, out_d):
     """Calls a shell script while looping over a given directory.
 
-    :param d: str - path to the directory with the BAM files that need to be analysed.
+    :param in_d: str - path to the directory with the BAM files that need to be analysed.
+    :param out_d: str - path to the output directory.
     """
-    for filename in os.listdir(d):
+    for filename in os.listdir(in_d):
         if filename.endswith(".bam"):
-            prefix = "/hpc/diaggen/users/Gabe/analysis/output_lobstr/allelotype/" + \
-                     filename.split("/")[-1].removesuffix(".bam")
-            subprocess.call(["/home/cog/gvandenhoeven/repos/PythonSTRing/lobSTR_script.sh", filename, prefix])
+            prefix = out_d + filename.split("/")[-1].removesuffix(".bam")
             subprocess.run(f"singularity shell -B /hpc/diaggen:/hpc/diaggen "
                            f"/hpc/diaggen/software/singularity_cache/lobster_v4.0.0.img")
             subprocess.run(f"allelotype --command classify --bam {filename} "
@@ -24,11 +23,9 @@ def analysis_pipeline(d):
 
 if __name__ == "__main_":
     print("Staring analysis pipeline...")
-    pc_dir, giab_dir = sys.argv[1], sys.argv[2]
     # pc_dir = "/hpc/diaggen/users/Gabe/data/wes/Positive_controls/"
     # giab_dir = "/hpc/diaggen/users/Gabe/data/wes/GIAB/"
-    analysis_pipeline(pc_dir)
-    analysis_pipeline(giab_dir)
+    analysis_pipeline(sys.argv[1], sys.argv[2])
     print("Analysis complete.")
 
 
