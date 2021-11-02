0ebd2778604d97279f9b55edaac3e5db9a85114b
GabevandenHoeven <55189858+GabevandenHoeven@users.noreply.github.com>
2021-11-02 09:50:53 +0100

    Delete sbatch_Pypeline.sh

--- sbatch_Pypeline.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-#SBATCH --time=4:00:00
-#SBATCH --cpus-per-task=4
-#SBATCH --mem=16
-#SBATCH --mail-user g.c.w.vandenhoeven-2@umcutrecht.nl
-#SBATCH --mail-type ALL
-
-module load python/3.6.1
-python /home/cog/gvandenhoeven/repos/PythonSTRing/$1 $2 $3
