8bc060f3e58ad48a914d27c605a71bba04cdd230
GabevandenHoeven <gcw.vandenhoeven@student.han.nl>
2021-11-08 09:55:31 +0100

    sbatch_Pypeline.sh aangepast; tijd en resources verminderd voor het runnen van Processing_LobSTR.py

--- sbatch_Pypeline.sh
+++ sbatch_Pypeline.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-#SBATCH --time=5:30:00
-#SBATCH --cpus-per-task=8
-#SBATCH --mem 32G
+#SBATCH --time=00:20:00
+#SBATCH --cpus-per-task=4
+#SBATCH --mem 16G
 #SBATCH --mail-user g.c.w.vandenhoeven-2@umcutrecht.nl
 #SBATCH --mail-type ALL
 
